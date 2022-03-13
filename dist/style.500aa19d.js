91c5007d4e51d8df017f34fc66e1b4d27bb6bd59
Alexandr Ilyin <sasha_1998_31@mail.ru>
2022-03-13 14:32:29 +0300

    picttures position fix

--- dist/style.500aa19d.js
+++ dist/style.500aa19d.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"C:\\Users\\Alex\\Desktop\\sasha_teach\\project\\src\\img\\search-icon.png":[["search-icon.d04ec39d.png","img/search-icon.png"],"img/search-icon.png"],"C:\\Users\\Alex\\Desktop\\sasha_teach\\project\\src\\img\\loft.png":[["loft.5b38c6e2.png","img/loft.png"],"img/loft.png"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"/Users/aleksandr/Desktop/tms/project/src/img/search-icon.png":[["search-icon.d04ec39d.png","img/search-icon.png"],"img/search-icon.png"],"/Users/aleksandr/Desktop/tms/project/src/img/loft.png":[["loft.5b38c6e2.png","img/loft.png"],"img/loft.png"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53824" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52863" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
